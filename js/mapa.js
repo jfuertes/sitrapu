2a07724bb379fd02d9b55f82b9b0ff76cd888457
Meka <mekatroniko@gmail.com>
2016-04-03 15:28:56 -0500

    agregados algo

--- js/mapa.js
+++ js/mapa.js
@@ -1,4 +1,16 @@
-jQuery(document).ready(function($) {
+function initMap(){
+	var latitude = -12.020345;
+	var longitude = -77.050299;
+	var opciones = {
+		zoom:14,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		center: new google.maps.LatLng(latitude, longitude)
+	};
+	var  canvas = $("#mapa")[0];
+	window.map = new google.maps.Map(canvas, opciones);
+}
 
-
+$(document).ready(function(){
+	//google.maps.event.addDomListener(window,'load',todoBien);
 });
+
